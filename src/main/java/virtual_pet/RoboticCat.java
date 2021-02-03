63b7bef5d696913aa195207a8bb6c2e8c0372dab
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-02-03 14:03:26 -0500

    Changes 2-3

---
+++ src/main/java/virtual_pet/RoboticCat.java
@@ -0,0 +1,20 @@
+package virtual_pet;
+
+public class RoboticCat extends RoboticPet {
+    private int batteryLevel;
+    private int oilLevel;
+    private int moodLevel;
+
+    public RoboticCat(String name, int batteryLevel, int oilLevel, int moodLevel){
+        super(name, batteryLevel, oilLevel, moodLevel);
+
+        this.batteryLevel = batteryLevel;
+        this.oilLevel = oilLevel;
+        this.moodLevel = moodLevel;
+
+    }
+
+    // Same situation as w/ dog we're going to need our overridden methods down here
+    // So whatever makes our roboticCat different from it's parent class really.
+
+}
---
+++ src/main/java/virtual_pet/RoboticDog.java
@@ -0,0 +1,21 @@
+//package virtual_pet;
+//
+//import javax.naming.Name;
+//
+//public class RoboticDog extends RoboticPet{
+//    private int batteryLevel;
+//    private int oilLevel;
+//    private int moodLevel;
+//
+//    public RoboticCat(String name, int batteryLevel, int oilLevel, int moodLevel){
+//        super(batteryLevel, oilLevel, moodLevel);
+//
+//        this.batteryLevel = batteryLevel;
+//        this.oilLevel = oilLevel;
+//        this.moodLevel = moodLevel;
+//    }
+
+    // Possible overrides here?
+    // Have to decide how we're going to handle.
+
+
