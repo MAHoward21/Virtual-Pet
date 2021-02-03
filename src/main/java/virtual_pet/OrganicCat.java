354878b193dc0c5c23cd8ba1e98e96575bf4c413
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-02-03 14:32:54 -0500

    Organic Dog&Cat

--- src/main/java/virtual_pet/OrganicCat.java
+++ src/main/java/virtual_pet/OrganicCat.java
@@ -1,4 +1,15 @@
-package virtual_pet;
-
-public class OrganicCat {
-}
+//package virtual_pet;
+//
+//public class OrganicCat extends OrganicPet{
+//    private int hungerStatus;
+//    private int thirstStatus;
+//    private int moodLevel;
+//
+//    public OrganicCat(String name, int hungerStatus, int thirstStatus, int moodLevel){
+//        super(int hungerStatus, thirstStatus, moodLevel);
+//    this.hungerStatus = hungerStatus;
+//    this.thirstStatus = thirstStatus;
+//    this.moodLevel = moodLevel;
+//}
+//
+////Methods to override other methods tailored to OrganicCat will go below this
--- src/main/java/virtual_pet/OrganicDog.java
+++ src/main/java/virtual_pet/OrganicDog.java
@@ -1,4 +1,18 @@
-package virtual_pet;
-
-public class OrganicDog {
-}
+//package virtual_pet;
+//
+//public class OrganicDog extends OrganicPet {
+//    private int hungerStatus;
+//    private int thirstStatus;
+//    private int moodLevel;
+//
+//    public OrganicDog(String name, int hungerStatus, int thirstStatus, int moodLevel){
+//        super(name, hungerStatus, thirstStatus, moodLevel);
+//
+//        this.hungerStatus = hungerStatus;
+//        this.thirstStatus = thirstStatus;
+//        this.moodLevel = moodLevel;
+//
+//    }
+//    // Methods that differentiate organic dog from others
+//
+//}
